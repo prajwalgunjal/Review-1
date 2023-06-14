ee9eed4646eeaa873e0fe277505afbb341eedc9f
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-14 15:47:44 +0530

    review code added

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Review1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int flag = 1;
+            int[] array = { 1, 2, 4, 7, 9, 10, 24, 31 };
+            for(int i = array.Length-1; i >=0; i--)
+            {
+                    if (array[i] % 2 != 0 && flag <= 2)
+                    {
+                        //Console.WriteLine(array[i]);
+                        flag++;
+                    }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
